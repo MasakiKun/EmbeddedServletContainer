2819f25195d854c40d3e42b08ca3b9bdbb8c448b
MasakiKun <kidconan@gmail.com>
2018-08-25 11:05:56 +0900

    .

--- Tomcat/src/main/java/kr/ayukawa/embeddservletcontainer/tomcat/EntryPoint.java
+++ Tomcat/src/main/java/kr/ayukawa/embeddservletcontainer/tomcat/EntryPoint.java
@@ -1,7 +1,12 @@
 package kr.ayukawa.embeddservletcontainer.tomcat;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.WebResource;
+import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+import org.apache.tomcat.util.scan.StandardJarScanner;
 
 import java.io.File;
 
@@ -23,6 +28,20 @@ public class EntryPoint {
 
 		// ContextPath 등록
 		Context ctx = tomcat.addWebapp("/", new File(WEBAPP_DIR).getAbsolutePath());
+
+/*
+		File additionWebInfClasses = new File("target/classes");
+		WebResourceRoot resources = new StandardRoot(ctx);
+		resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(), "/"));
+		ctx.setResources(resources);
+*/
+/*
+		StandardJarScanner scanner = (StandardJarScanner)ctx.getJarScanner();
+		scanner.setScanClassPath(true);
+		scanner.setScanBootstrapClassPath(true);
+		scanner.setScanAllDirectories(true);
+		scanner.setScanAllFiles(true);
+*/
 
 		// Servlet 등록
 		tomcat.addServlet(ctx, "helloServlet", new kr.ayukawa.embeddservletcontainer.servlet.HelloServlet());
